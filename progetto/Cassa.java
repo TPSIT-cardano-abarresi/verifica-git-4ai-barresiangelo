77711eafc454e8964ebef5aadc621578f589ade1
abarresiC <abarresi@itiscardanopv.edu.it>
2026-02-18 14:44:49 +0100

    Create Cassa.java

---
+++ progetto/Cassa.java
@@ -0,0 +1,19 @@
+pblic class Cassa {
+
+  public void AggiungiProdotto(int ){
+    System.out.print("La scelta effettuata è" + scelta);
+    System.out.print("Aggiungi caffé");
+    System.out.print("Aggiungi acqua");
+    System.out.print("Aggiungi cornetto");
+  }
+
+  public double getTotale(){
+    return totale;
+  }
+
+  public void paga(soldi){
+
+    //da fare
+  }
+  
+}
